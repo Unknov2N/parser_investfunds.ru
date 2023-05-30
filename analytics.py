ad6827593c5eed9b52f3745b83cd4eb5135318a8
Шипулин Павел <U2n_099@mail.ru>
2023-05-30 20:57:48 +0700

    добавил корректный построчный вывод интересующих корреляций (но немного с костылями)

--- analytics.py
+++ analytics.py
@@ -16,16 +16,26 @@ num_data = {key: input_data[key] for key in input_data
 del num_data['Unnamed: 0']
 df = pd.DataFrame(data=num_data)
 corr = df.corr()
+labels = df.columns
+corr_ = corr.values.tolist()
+for i in range(0, len(corr_)-1):
+    for j in range(i+1, len(corr_)):
+        corr_[i][j] = round(corr_[i][j], 3)
+        if 0.9999999 > corr_[i][j] > COVAR_THRESHOLD or corr_[i][j] < -COVAR_THRESHOLD:
+            print(labels[i], labels[j], corr_[i][j])
+
+# другой вариант, более красивый, но более затратный и менее читаемый
 for key in corr:
     i = 0
     for item in corr[key]:
         corr[key] = round(corr[key], 3)
         if 0.9999999 > item > COVAR_THRESHOLD or item < -COVAR_THRESHOLD:
-            print(key, item)
+            # print(key, labels[j], item)
             i += 1
     if i == 0:
-        corr.drop(columns=key, inplace=True)
-        corr.drop(labels=key, axis=0, inplace=True)
+       pass
+       corr.drop(columns=key, inplace=True)
+       corr.drop(labels=key, axis=0, inplace=True)
 
 # visualization
 sns.set(rc={"figure.figsize": (35, 14)})
